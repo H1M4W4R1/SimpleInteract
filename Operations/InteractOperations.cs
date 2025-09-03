b10de8ecd88465039e21d82a5d2938e287b50f79
Patryk Pastuszka <nov11157@gmail.com>
2025-09-03 13:05:36 +0200

    Minor fixes

--- Operations/InteractOperations.cs
+++ Operations/InteractOperations.cs
@@ -4,7 +4,7 @@ namespace Systems.SimpleInteract.Operations
 {
     public sealed class InteractOperations
     {
-        public const ushort SYSTEM_INTERACTION = 0x0006;
+        public const ushort SYSTEM_INTERACTION = 0x0007;
 
         public static OperationResult Permitted()
             => OperationResult.Success(SYSTEM_INTERACTION, OperationResult.SUCCESS_PERMITTED);
